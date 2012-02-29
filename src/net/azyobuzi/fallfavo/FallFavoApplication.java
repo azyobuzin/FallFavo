6b92a6d3fbec7f10bd79d3a0a1dcccd4a2e3fee4
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-02-29 21:19:14 +0900

    HTTPSと使うようにした, 同時実行数設定のデフォルト値（1）を設定

--- src/net/azyobuzi/fallfavo/FallFavoApplication.java
+++ src/net/azyobuzi/fallfavo/FallFavoApplication.java
@@ -12,11 +12,12 @@ public class FallFavoApplication extends Application {
 	public static FallFavoApplication getInstance() {
 		return instance;
 	}
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
+		System.setProperty("twitter4j.http.useSSL", "true");
 		System.setProperty("twitter4j.oauth.consumerKey", "xnrEMcjtIbQWiXkOerL3gQ");
 		System.setProperty("twitter4j.oauth.consumerSecret", "YV9wsZAndTrNeaQA1JYhU8hdtFcPM7i2F39s55iaZmw");
 	}
